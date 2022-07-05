16fb14be469b377916c29809312ab0f75ea0a9b6
Avram Vlad <vlad@192.168.1.3>
2022-07-05 13:07:09 +0300

    added tailwind

--- app/root.tsx
+++ app/root.tsx
@@ -1,5 +1,8 @@
-import type { MetaFunction } from '@remix-run/node';
+import type { MetaFunction, LinksFunction } from '@remix-run/node';
 import { Links, LiveReload, Meta, Outlet, Scripts, ScrollRestoration } from '@remix-run/react';
+import styles from './styles/app.css';
+
+export const links: LinksFunction = () => [{ rel: 'stylesheet', href: styles }];
 
 export const meta: MetaFunction = () => ({
   charset: 'utf-8',
