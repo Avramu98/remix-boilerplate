c45c441af4acd2de41c400554b3fccad8a998d76
Avramu98 <vlad_avram98@yahoo.com>
2022-07-26 22:32:24 +0300

    added example of external api request

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -3,18 +3,23 @@ import { getUser } from '~/utils/auth.server';
 import { Layout } from '~/components/layout';
 import { Outlet, useLoaderData } from '@remix-run/react';
 import { json } from '@remix-run/node';
+import { getProjects, getTasks } from '~/utils/meisterTask.server';
 
 export const loader: LoaderFunction = async ({ request, params }) => {
   const user = await getUser(request);
+  const meisterData = await getProjects();
+  const meisterTasks = await getTasks(meisterData[1]);
   if (user) {
-    return json({ user });
+    return json({ user, meisterTasks });
   }
   return null;
 };
 
 const Home = () => {
-  const { user } = useLoaderData();
+  const { user, meisterTasks } = useLoaderData();
   const { firstName, lastName } = user.profile;
+  console.log(meisterTasks);
+
   return (
     <Layout>
       <div className="p-6 bg-gray-300 flex md:justify-between lg:justify-between text-center justify-center  content-center flex-wrap items-center gap-3">
